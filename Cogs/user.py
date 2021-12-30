e8f746f90cee86bdef039901bf02c58e3d821498
Learning Riven <87784388+LearningRiven@users.noreply.github.com>
2021-12-30 03:26:10 +0000

    added comments

--- Cogs/user.py
+++ Cogs/user.py
@@ -19,14 +19,18 @@ class User(commands.Cog):
   #Autorole Method - uses on_member_join in order to give a role to everyone 
   @commands.Cog.listener()
   async def on_member_join(self,member):
+    #Add default role
     role = get(member.guild.roles, name=DEFAULT_ROLES)
     await member.add_roles(role)
     print(f"USER: {member} has joined the server and was given {role}")
+
+    #log to the admin logging channel
     channel = member.guild.get_channel(925933545854222356);
     await channel.send(f"{member} has left the server. We now have {member.guild.member_count} members");
 
   @commands.Cog.listener()
   async def on_member_remove(self,member):
+    #log to the admin logging channel
     channel = member.guild.get_channel(925933545854222356);
     await channel.send(f"{member} has left the server. We now have {member.guild.member_count} members");
 
